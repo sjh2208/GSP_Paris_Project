5885ddef73d10bcab7eb135fb76486ca47bc3ada
Samuel Hutchinson <93997857+sjh2208@users.noreply.github.com>
2022-06-03 13:46:08 +0200

    Add files via upload

--- stims/gen_stim.py
+++ stims/gen_stim.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 from PIL import Image
@@ -35,14 +35,6 @@ def fix_elem(img):
 
 #set up template files for combination later
 grid = Image.open('./templates/grid.jpeg').convert('RGBA')
-tri = Image.open('./templates/triangle.jpeg').convert('RGBA')
-tri = fix_elem(tri)
-sq = Image.open('./templates/square.jpeg').convert('RGBA')
-sq = fix_elem(sq)
-pent = Image.open('./templates/pentagon.jpeg').convert('RGBA')
-pent = fix_elem(pent)
-pie = Image.open('./templates/pie.jpeg').convert('RGBA')
-pie = fix_elem(pie)
 a = Image.open('./templates/a.jpeg').convert('RGBA')
 a = fix_elem(a)
 b = Image.open('./templates/b.jpeg').convert('RGBA')
@@ -51,6 +43,14 @@ c = Image.open('./templates/c.jpeg').convert('RGBA')
 c = fix_elem(c)
 d = Image.open('./templates/d.jpeg').convert('RGBA')
 d = fix_elem(d)
+e = Image.open('./templates/e.jpeg').convert('RGBA')
+e = fix_elem(e)
+f = Image.open('./templates/f.jpeg').convert('RGBA')
+f = fix_elem(f)
+g = Image.open('./templates/g.jpeg').convert('RGBA')
+g = fix_elem(g)
+h = Image.open('./templates/h.jpeg').convert('RGBA')
+h = fix_elem(h)
 one = Image.open('./templates/1.jpeg').convert('RGBA')
 one = fix_elem(one)
 two = Image.open('./templates/2.jpeg').convert('RGBA')
@@ -59,13 +59,21 @@ three = Image.open('./templates/3.jpeg').convert('RGBA')
 three = fix_elem(three)
 four = Image.open('./templates/4.jpeg').convert('RGBA')
 four = fix_elem(four)
+five = Image.open('./templates/five.jpeg').convert('RGBA')
+five = fix_elem(d)
+six = Image.open('./templates/six.jpeg').convert('RGBA')
+six = fix_elem(six)
+seven = Image.open('./templates/seven.jpeg').convert('RGBA')
+seven = fix_elem(seven)
+eight = Image.open('./templates/eight.jpeg').convert('RGBA')
+eight = fix_elem(eight)
 
 #create new stims
 path_name = './outputs/stim_{f}_{n}.png'
 max = 100
 
 #which-figure loop
-for i in range(3):
+for i in range(2):
     
     #how-many-grids-of-each loop
     for j in range(0, max):
@@ -73,49 +81,14 @@ for i in range(3):
         new_grid = grid.copy()
         new_boxes = boxes.copy()
         
-        if i == 0: #figure=shape
+            
+        if i == 0: #figure=letter
             
             #fill-in-boxes loops
             for s1 in range(8):
                 box = new_boxes[np.random.randint(0, len(new_boxes))]
                 new_boxes.remove(box)
-                shape = np.random.randint(0, 4)
-                if shape == 0:
-                    new_grid.paste(tri, coords[box], tri)
-                elif shape == 1:
-                    new_grid.paste(sq, coords[box], sq)
-                elif shape == 2:
-                    new_grid.paste(pent, coords[box], pent)
-                elif shape == 3:
-                    new_grid.paste(pie, coords[box], pie)
-            for s2 in range(4):
-                box = new_boxes[np.random.randint(0, len(new_boxes))]
-                new_boxes.remove(box)
-                fig = np.random.randint(0, 8)
-                if fig == 0:
-                    new_grid.paste(a, coords[box], a)
-                elif fig == 1:
-                    new_grid.paste(b, coords[box], b)
-                elif fig == 2:
-                    new_grid.paste(c, coords[box], c)
-                elif fig == 3:
-                    new_grid.paste(d, coords[box], d)
-                elif fig == 4:
-                    new_grid.paste(one, coords[box], one)
-                elif fig == 5:
-                    new_grid.paste(two, coords[box], two)
-                elif fig == 6:
-                    new_grid.paste(three, coords[box], three)
-                elif fig == 7:
-                    new_grid.paste(four, coords[box], four)
-            
-        elif i == 1: #figure=letter
-            
-            #fill-in-boxes loops
-            for s1 in range(8):
-                box = new_boxes[np.random.randint(0, len(new_boxes))]
-                new_boxes.remove(box)
-                letter = np.random.randint(0, 4)
+                letter = np.random.randint(0, 8)
                 if letter == 0:
                     new_grid.paste(a, coords[box], a)
                 elif letter == 1:
@@ -124,34 +97,42 @@ for i in range(3):
                     new_grid.paste(c, coords[box], c)
                 elif letter == 3:
                     new_grid.paste(d, coords[box], d)
+                elif letter == 4:
+                    new_grid.paste(e, coords[box], e)
+                elif letter == 5:
+                    new_grid.paste(f, coords[box], f)
+                elif letter == 6:
+                    new_grid.paste(g, coords[box], g)
+                elif letter == 7:
+                    new_grid.paste(h, coords[box], h)
             for s2 in range(4):
                 box = new_boxes[np.random.randint(0, len(new_boxes))]
                 new_boxes.remove(box)
                 fig = np.random.randint(0, 8)
                 if fig == 0:
-                    new_grid.paste(tri, coords[box], tri)
-                elif fig == 1:
-                    new_grid.paste(sq, coords[box], sq)
-                elif fig == 2:
-                    new_grid.paste(pent, coords[box], pent)
-                elif fig == 3:
-                    new_grid.paste(pie, coords[box], pie)
-                elif fig == 4:
                     new_grid.paste(one, coords[box], one)
-                elif fig == 5:
+                elif fig == 1:
                     new_grid.paste(two, coords[box], two)
-                elif fig == 6:
+                elif fig == 2:
                     new_grid.paste(three, coords[box], three)
-                elif fig == 7:
+                elif fig == 3:
                     new_grid.paste(four, coords[box], four)
+                elif fig == 4:
+                    new_grid.paste(five, coords[box], five)
+                elif fig == 5:
+                    new_grid.paste(six, coords[box], six)
+                elif fig == 6:
+                    new_grid.paste(seven, coords[box], seven)
+                elif fig == 7:
+                    new_grid.paste(eight, coords[box], eight)
             
-        elif i == 2: #figure=number
+        elif i == 1: #figure=number
             
             #fill-in-boxes loops
             for s1 in range(8):
                 box = new_boxes[np.random.randint(0, len(new_boxes))]
                 new_boxes.remove(box)
-                number = np.random.randint(0, 4)
+                number = np.random.randint(0, 8)
                 if number == 0:
                     new_grid.paste(one, coords[box], one)
                 elif number == 1:
@@ -160,6 +141,14 @@ for i in range(3):
                     new_grid.paste(three, coords[box], three)
                 elif number == 3:
                     new_grid.paste(four, coords[box], four)
+                elif number == 4:
+                    new_grid.paste(five, coords[box], five)
+                elif number == 5:
+                    new_grid.paste(six, coords[box], six)
+                elif number == 6:
+                    new_grid.paste(seven, coords[box], seven)
+                elif number == 7:
+                    new_grid.paste(eight, coords[box], eight)
             for s2 in range(4):
                 box = new_boxes[np.random.randint(0, len(new_boxes))]
                 new_boxes.remove(box)
@@ -173,23 +162,17 @@ for i in range(3):
                 elif fig == 3:
                     new_grid.paste(d, coords[box], d)
                 elif fig == 4:
-                    new_grid.paste(tri, coords[box], tri)
+                    new_grid.paste(e, coords[box], e)
                 elif fig == 5:
-                    new_grid.paste(sq, coords[box], sq)
+                    new_grid.paste(f, coords[box], f)
                 elif fig == 6:
-                    new_grid.paste(pent, coords[box], pent)
+                    new_grid.paste(g, coords[box], g)
                 elif fig == 7:
-                    new_grid.paste(pie, coords[box], pie)
+                    new_grid.paste(h, coords[box], h)
 
         #crop and save the final stim
         new_grid = new_grid.crop((41, 29, 382, 285))
         new_grid.save(path_name.format(f=i, n=j), 'PNG')
         
 print('Done generating stimuli!')
-
-
-# In[ ]:
-
-
-
 
